9a3de9954204ec915fb900d59aa2befa15d1b321
fahmipraga <itmamul2004@gmail.com>
2023-06-17 17:29:20 +0700

    fix: auth

--- src/helpers/errorhandler.js
+++ src/helpers/errorhandler.js
@@ -1,8 +1,44 @@
 export default function errorHandler(res, err) {
-  if (err?.message?.includes("not_found")) {
+  if (err?.message?.includes("auth_wrong_password")) {
     return res.status(404).json({
       success: false,
-      message: "Error, data is not found",
+      message: "auth_wrong_password",
+    })
+  }
+  if (err?.message?.includes("auth_no_email")) {
+    return res.status(404).json({
+      success: false,
+      message: "auth_no_email",
+    })
+  }
+  if (err?.message?.includes("auth_no_user")) {
+    return res.status(404).json({
+      success: false,
+      message: "auth_no_user",
+    })
+  }
+  if (err?.message?.includes("auth_forgot_password_fail")) {
+    return res.status(404).json({
+      success: false,
+      message: "auth_forgot_password_fail",
+    })
+  }
+  if (err?.message?.includes("auth_code_invalid")) {
+    return res.status(404).json({
+      success: false,
+      message: "auth_code_invalid",
+    })
+  }
+  if (err?.message?.includes("auth_no_forgot_request")) {
+    return res.status(404).json({
+      success: false,
+      message: "auth_no_forgot_request",
+    })
+  } else {
+    console.log(err)
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
     })
   }
 }
--- src/routers/auth/auth.router.js
+++ src/routers/auth/auth.router.js
@@ -1,10 +1,17 @@
 import { Router } from "express"
-import { Login } from "../../controllers/auth/auth.controller"
-import { Register } from "../../controllers/auth/auth.controller"
+import {
+  Login,
+  Register,
+  ForgotPassword,
+  ResetPassword,
+} from "../../controllers/auth/auth.controller.js"
+import { validate } from "../../middlewares/validator.middleware.js"
 
 const authRouter = Router()
 
-authRouter.post("/login", Login)
-authRouter.post("/register", Register)
+authRouter.post("/login", validate("authLogin"), Login)
+authRouter.post("/register", validate("authRegister"), Register)
+authRouter.post("/forgot-password", validate("authForgot"), ForgotPassword)
+authRouter.post("/reset-password", validate("authReset"), ResetPassword)
 
 export default authRouter
