c2c3c6503e45eecb55003b44f6622f48e48c1cca
fahmipraga <itmamul2004@gmail.com>
2023-06-18 02:05:01 +0700

    feat: add morgan

--- src/routers/product.router.js
+++ src/routers/product.router.js
@@ -1,7 +1,8 @@
 import { Router } from "express"
+import * as ProductController from "../controllers/product.controller.js"
 
 const ProductRouter = Router()
 
-ProductRouter.get("/")
+ProductRouter.get("/", ProductController.getAll)
 
 export default ProductRouter
