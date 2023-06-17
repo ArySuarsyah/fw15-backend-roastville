98d50e80dd75497b7e9571d8137edf7da44bb632
fahmipraga <itmamul2004@gmail.com>
2023-06-18 02:33:17 +0700

    feat: add product

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -13,3 +13,16 @@ export const getAll = async function (req, res) {
     return errorHandler(res, err)
   }
 }
+
+export const Insert = async function (req, res) {
+  try {
+    const product = await ProductModel.Insert(req.body)
+    return res.json({
+      success: true,
+      message: "Get all products successfully",
+      results: product,
+    })
+  } catch (err) {
+    return errorHandler(res, err)
+  }
+}
--- src/routers/product.router.js
+++ src/routers/product.router.js
@@ -4,5 +4,6 @@ import * as ProductController from "../controllers/product.controller.js"
 const ProductRouter = Router()
 
 ProductRouter.get("/", ProductController.getAll)
+ProductRouter.post("/", ProductController.Insert)
 
 export default ProductRouter
