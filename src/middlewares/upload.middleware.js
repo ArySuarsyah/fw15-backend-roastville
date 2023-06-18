4dd53599466ece09e2fa7d00645c82f6bab716e4
luthfimauludy <putra.luthfi@gmail.com>
2023-06-18 16:32:12 +0700

    fix: using try and catch

--- src/middlewares/upload.middleware.js
+++ src/middlewares/upload.middleware.js
@@ -1,24 +1,24 @@
-import multer  from "multer"
-import { v2 as cloudinary}  from "cloudinary"
+import multer from "multer"
+import { v2 as cloudinary } from "cloudinary"
 import { CloudinaryStorage } from "multer-storage-cloudinary"
 
-// Configuration 
+// Configuration
 cloudinary.config({
-    cloud_name: "dxs0yxeyr",
-    api_key: "236157336681252",
-    api_secret: "V2uHsegpJtBpFlUl3WSwkxdCL0I"
+  cloud_name: "dxs0yxeyr",
+  api_key: "236157336681252",
+  api_secret: "V2uHsegpJtBpFlUl3WSwkxdCL0I",
 })
 
 const storage = new CloudinaryStorage({
-    cloudinary: cloudinary,
-    params: {
-        folder: "YogiPrayoga",
-        format: async (req, file) => "png", // supports promises as well
-        public_id: (req, file) => {
-            const filename = new Date().getTime().toString()
-            return filename
-        },
+  cloudinary: cloudinary,
+  params: {
+    folder: "YogiPrayoga",
+    format: async (req, file) => "png", // supports promises as well
+    public_id: (req, file) => {
+      const filename = new Date().getTime().toString()
+      return filename
     },
+  },
 })
 
 // const storage = multer.diskStorage({
@@ -33,40 +33,38 @@ const storage = new CloudinaryStorage({
 //     }
 // })
 
-const limits =  {
-    fileSize: 1 * 1024 * 1024
+const limits = {
+  fileSize: 1 * 1024 * 1024,
 }
 
-const fileFilter = (req, file, cb) =>{
-    const formatFile = ["image/jpg", "image/jpeg"]
-    if(!formatFile.includes(file.mimetype)){
-        console.log(file.mimetype)
-        cb(Error("fileformat_error"))
-    }
-    cb(null,true)
+const fileFilter = (req, file, cb) => {
+  const formatFile = ["image/jpg", "image/jpeg"]
+  if (!formatFile.includes(file.mimetype)) {
+    console.log(file.mimetype)
+    cb(Error("fileformat_error"))
+  }
+  cb(null, true)
 }
 
-const upload = multer({storage, limits, fileFilter})
+const upload = multer({ storage, limits, fileFilter })
 
 const uploadMiddleware = (field) => {
-    const uploadField = upload.single(field)
-    return (request, response, next) => {
-        uploadField(request, response, (err) => {
-            if(err){
-                if(err.message === "fileformat_error"){
-                    return response.status(400).json({
-                        success: false,
-                        message: "File format invalid"
-                    })
-                }
-                return response.status(400).json({
-                    success: false,
-                    message: "File to large"
-                })
-            }
-            return next()
-        })
-    }
+  const uploadField = upload.single(field)
+  return (req, res, next) => {
+    uploadField(req, res, (err) => {
+      try {
+        if (err) {
+          if (err.message === "fileformat_error") {
+            throw Error("fileformat_error")
+          }
+          throw Error(err.message)
+        }
+        return next()
+      } catch (err) {
+        return errorHandler(res, err)
+      }
+    })
+  }
 }
 
-export default uploadMiddleware;
+export default uploadMiddleware
