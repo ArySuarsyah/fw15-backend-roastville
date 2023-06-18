3ca7a9703e9236b25f6d7f54d0bcc98fda6d5a95
luthfimauludy <putra.luthfi@gmail.com>
2023-06-18 16:34:57 +0700

    update: upload middleware

--- src/routers/products.router.js
+++ src/routers/products.router.js
@@ -1,9 +1,14 @@
 import { Router } from "express"
 import * as ProductController from "../controllers/products.controller.js"
+import uploadMiddleware from "../middlewares/upload.middleware.js"
 
 const ProductRouter = Router()
 
 ProductRouter.get("/", ProductController.getAll)
-ProductRouter.post("/", ProductController.Insert)
+ProductRouter.post(
+  "/",
+  uploadMiddleware("picture"),
+  ProductController.createProduct
+)
 
 export default ProductRouter
