46ab4b3ae7b126f7ef3166f18e4845506f20a1c4
luthfimauludy <putra.luthfi@gmail.com>
2023-06-19 13:23:14 +0700

    update: find one and fine one by id and variant

--- src/models/products.model.js
+++ src/models/products.model.js
@@ -12,6 +12,34 @@ export const findAllProduct = async function () {
   return rows
 }
 
+export const findOne = async (id) => {
+  const query = `
+  SELECT * FROM ${table}
+  CROSS JOIN jsonb_array_elements("variant") AS "sku"
+  WHERE "id" = $1
+  `
+  const values = [id]
+  const { rows } = await db.query(query, values)
+  return rows
+}
+
+export const findOneByIdAndVariant = async (id, code) => {
+  const query = `
+  SELECT 
+  "id",
+  "name",
+  "picture",
+  "description",
+  "sku"
+  FROM ${table}
+  CROSS JOIN jsonb_array_elements("variant") AS "sku"
+  WHERE "sku"->> 'code' = ANY($2) AND "id" = ANY($1);
+  `
+  const values = [id, code]
+  const { rows } = await db.query(query, values)
+  return rows
+}
+
 export const insert = async function (data) {
   const query = `
   INSERT INTO ${table} ("picture", "name", "description", "variant")
