255d0e99dfa58197deaad4dd2f863f07bab03ade
fahmipraga <itmamul2004@gmail.com>
2023-06-21 03:24:58 +0700

    fix: fix history

--- src/models/history.model.js
+++ src/models/history.model.js
@@ -12,11 +12,11 @@ export const getHistory = async (id) => {
 export const makeHistory = async (id, data) => {
   const query = `
     INSERT INTO "history" 
-    ("userid", "name", "price") 
-    VALUES ($1, $2, $3) RETURNING *
+    ("userid", "name", "price", "picture") 
+    VALUES ($1, $2, $3, $4) RETURNING *
     `
 
-  const values = [id, data.name, data.price]
+  const values = [id, data.name, data.price, data.picture]
   const { rows } = await db.query(query, values)
   return rows[0]
 }
