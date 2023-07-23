42e3161c544fc7910417c24d98d8e3a8c93d38b3
fahmipraga <itmamul2004@gmail.com>
2023-07-23 13:16:10 +0700

    fix: delete history router

--- src/routers/history.router.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express"
-import * as historyController from "../controllers/history.controller.js"
-
-const historyRouter = Router()
-
-historyRouter.get("/", historyController.getAllHistory)
-historyRouter.post("/", historyController.makeHistory)
-
-export default historyRouter
