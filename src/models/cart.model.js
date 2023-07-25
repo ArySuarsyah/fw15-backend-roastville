6b256957ed6443ffebb9416620fe5e906dcaea46
fahmipraga <itmamul2004@gmail.com>
2023-07-25 14:31:35 +0700

    fix: cart logic

--- src/models/cart.model.js
+++ src/models/cart.model.js
@@ -1,13 +1,29 @@
 import db from "../helpers/db.helper.js"
 const table = "cart"
 
-export const getCart = async () => {
+export const getCart = async (id) => {
   const query = `
-  SELECT * FROM ${table}
+  SELECT
+  "p"."id",
+  "p"."name",
+  "p"."picture",
+  "p"."description"
+  FROM ${table}
+  JOIN "products" "p" ON "p"."id" = ${table}."productId"
+  WHERE ${table}."userId" = $1;
   `
-  const values = []
+  const values = [id]
   const { rows } = await db.query(query, values)
   return rows
+}
+
+export const getOneCart = async (data, id) => {
+  const query = `
+  SELECT * FROM ${table} WHERE "productId" = $1 AND "userId" = $2
+  `
+  const values = [data, id]
+  const { rows } = await db.query(query, values)
+  return rows[0]
 }
 
 export const insertCart = async (data, id) => {
@@ -16,7 +32,18 @@ export const insertCart = async (data, id) => {
   VALUES ($1, $2) RETURNING *
   `
 
-  const values = [data.productId, id]
+  const values = [data, id]
+  const { rows } = await db.query(query, values)
+  return rows
+}
+
+export const destroyCart = async (data, id) => {
+  const query = `
+  DELETE FROM ${table} WHERE "productId" = $1 AND "userId" = $2
+  RETURNING *
+  `
+
+  const values = [data, id]
   const { rows } = await db.query(query, values)
   return rows
 }
--- src/routers/cart.router.js
+++ src/routers/cart.router.js
@@ -1,9 +1,9 @@
 import { Router } from "express"
 import * as CartController from "../controllers/cart.controller.js"
-import uploadMiddleware from "../middlewares/upload.middleware.js"
 
 const CartRouter = Router()
 
+CartRouter.get("/:id", CartController.getOneCart)
 CartRouter.get("/", CartController.getAllCart)
 CartRouter.post("/", CartController.insertCart)
 
